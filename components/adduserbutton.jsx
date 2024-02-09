72d41c46dfeb32bb4d629dccd5360e6b193c625e
azadtom <kumarazad2917@gmail.com>
2024-02-09 14:01:20 +0530

    adjust add button

--- components/adduserbutton.jsx
+++ components/adduserbutton.jsx
@@ -19,7 +19,7 @@ export const AddUser = () => {
 
   return (
     <>
-      {!form && <div className="fixed right-4 bottom-4">
+      {!form && <div className="fixed right-4 bottom-16 sm:bottom-8">
         <button onClick={openUserForm} className="flex items-center  gap-1 bg-gray-800 px-4 py-2 hover:border hover:border-blue-600 active:focus:border-white">
           <span className="text-2xl">+</span>
           <span>ADD USER</span>
